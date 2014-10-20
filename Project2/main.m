f76c0ca5f12efef5df293c7db6a448ab7190b3e3
Ubivin <ubivin@gmail.com>
2014-10-19 19:52:43 -0700

    Initial Commit

---
+++ Project2/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Project2
+//
+//  Created by Jason Hoang on 10/19/14.
+//  Copyright (c) 2014 Jason Hoang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
